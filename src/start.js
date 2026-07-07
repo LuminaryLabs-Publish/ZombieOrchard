306a6445774b12e654af676055fa08fbe283de26
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-06 21:41:49 -0400

    Wire browser start module

--- src/start.js
+++ src/start.js
@@ -1,1 +1,17 @@
-import "./game.js";
+import { createOrchardGame } from "./game.js";
+import { createHtmlInterfaceRenderer } from "./renderer/html-interface-renderer.js";
+import { createWorldCanvas } from "./renderer/world-canvas.js";
+
+const engine = createOrchardGame();
+const world = createWorldCanvas(document.querySelector("#world"));
+const ui = createHtmlInterfaceRenderer({ root: document.querySelector("#ui-root"), engine });
+
+function draw() {
+  const snapshot = engine.tick(1 / 60);
+  world.render(snapshot);
+  ui.render(snapshot);
+  requestAnimationFrame(draw);
+}
+
+window.GameHost = { engine, getState: () => engine.snapshot(), tick: (dt) => engine.tick(dt) };
+draw();
